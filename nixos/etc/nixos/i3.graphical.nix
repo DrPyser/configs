396eadc55046f1e9baffcf4f1a53072c2de79861
Charles Langlois <schok53@gmail.com>
2022-02-03 23:35:42 -0500

    Updated nixos i3 module

--- nixos/etc/nixos/i3.graphical.nix
+++ nixos/etc/nixos/i3.graphical.nix
@@ -7,19 +7,26 @@
     pkgs.xdotool
     pkgs.libnotify
     pkgs.dunst
-		pkgs.i3
-		pkgs.i3blocks
+    pkgs.i3
+    pkgs.i3blocks
   ];
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # set keyboard settings
   services.xserver.layout = "ca";
   services.xserver.xkbOptions = "caps:super";
 
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
 
+  # enable i3 as windows manager
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.windowManager.i3.enable = true;
-  services.xserver.displayManager.defaultSession = "none+i3";
+
+  # for root environment
+  # perhaps should be defined elsewhere
+  environment.variables.EDITOR = "kak";
+  environment.variables.TERMINAL = "st";
 }
